8d78da6573ce62fc051ba1441c73fcfe6fc2bc18
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-05 19:16:31 +0100

    reafactor: add tailwind reusable colors to extends in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      primary: "#030014",
+      accent: "#AB8BFF",
+      secondary: "#151312",
+      light: {
+        100: '#D6C6FF',
+        200: '#A8B5DB',
+        300: '#9CA4AB',
+      }, 
+      dark: {
+        100: '221f3d',
+        200: '#0f0d23'
+      }
+    },
   },
   plugins: [],
 }
