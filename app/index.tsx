4e1db1c7a9c0073d0d6f6a6b60f17a25af9bf962
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-05 19:18:35 +0100

    style: rplace style to className in View

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View className="flex-1 justify-center items-center"
     >
-      <Text>Welcome .</Text>
+      <Text className="text-primary text-2xl font-bold">Welcome Meshach.</Text>
+      
     </View>
   );
 }
