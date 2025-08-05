f302d7051c8a0cbbca5c018a7ad5b82ad02dc7cf
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-05 22:06:11 +0100

    feat: add Meshach to Welcome

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center"
+    >
+      <Text className="text-primary text-2xl font-bold">Welcome Meshach.</Text>
+      
+    </View>
+  );
+}
