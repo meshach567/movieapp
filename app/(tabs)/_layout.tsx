47dee2d0d3bf9832c3c72e612b62b3dbcd0e9d7d
Meshach Arinze <arinzemeshach123@gmail.com>
2025-08-05 22:04:48 +0100

    fix: capitalise the layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
 
-const _layout = () => {
+const _Layout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="search" options={{ title: 'Search' }} />
-      <Tabs.Screen name="saved" options={{ title: 'Saved' }} />
-      <Tabs.Screen name="profile" options={{ title: 'Profile' }} />
+      <Tabs.Screen name="index" options={{ title: 'Home', headerShown: false }} />
+      <Tabs.Screen name="search" options={{ title: 'Search', headerShown: false }} />
+      <Tabs.Screen name="saved" options={{ title: 'Saved', headerShown: false }} />
+      <Tabs.Screen name="profile" options={{ title: 'Profile', headerShown: false }} />
     </Tabs>
   )
 }
 
-export default _layout
+export default _Layout
